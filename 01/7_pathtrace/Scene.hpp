90a9027168873091b07c32f11fa94db55f2d2984
nuisancedm <nuisancedm@gmail.com>
2024-03-06 21:38:26 +0800

    .

--- 01/7_pathtrace/Scene.hpp
+++ 01/7_pathtrace/Scene.hpp
@@ -17,7 +17,7 @@ class Scene
 {
 public:
     // setting up options
-    int width = 1280;
+    int width = 1280; //
     int height = 960;
     double fov = 40;
     Vector3f backgroundColor = Vector3f(0.235294, 0.67451, 0.843137);
